5c58c72805dc59ec56d4c20642bd8d74d3322408
theskidster <theskidster@gmail.com>
2021-02-07 16:35:08 -0600

    added mesh to geometry- needs some refactoring

--- src/dev/theskidster/mapeditor/world/GeometryBatch.java
+++ src/dev/theskidster/mapeditor/world/GeometryBatch.java
@@ -2,10 +2,15 @@ package dev.theskidster.mapeditor.world;
 
 import dev.theskidster.mapeditor.graphics.Graphics;
 import dev.theskidster.mapeditor.main.App;
+import dev.theskidster.mapeditor.main.LogLevel;
+import dev.theskidster.mapeditor.main.Logger;
 import dev.theskidster.mapeditor.main.ShaderProgram;
+import java.nio.BufferOverflowException;
+import org.joml.Vector2f;
 import org.joml.Vector3f;
 import static org.lwjgl.opengl.GL30.*;
 import org.lwjgl.system.MemoryUtil;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 /**
  * @author J Hoffman
@@ -14,41 +19,47 @@ import org.lwjgl.system.MemoryUtil;
 
 class GeometryBatch {
     
-    private final int numFloats = 24;
-    private final int numFloatsPerVertex = 3;
+    private final int numFloats = 40;
+    private final int numFloatsPerVertex = 5;
     private int numVertices;
     private int prevNumShapes;
     
     private final Graphics g = new Graphics();
     
-    private void render(ShaderProgram program) {
-        glPointSize(4);
-        
+    private void render(ShaderProgram program) {        
         glBindVertexArray(g.vao);
         
         glBindBuffer(GL_ARRAY_BUFFER, g.vbo);
         glBufferSubData(GL_ARRAY_BUFFER, 0, g.vertices);
         
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g.ibo);        
+        glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, g.indices);
+        
         program.setUniform("uType", 2);
         
-        glDrawArrays(GL_POINTS, 0, numVertices / numFloatsPerVertex);
+        glDrawElements(GL_TRIANGLES, g.indices.limit() * (numVertices / numFloatsPerVertex), GL_UNSIGNED_INT, NULL);
         
-        glPointSize(1);
         App.checkGLError();
     }
     
     void batchStart(int numShapes) {
         if(numShapes != prevNumShapes) {
             g.vertices = MemoryUtil.memAllocFloat(numFloats * numShapes * Float.BYTES);
-        
+            g.indices  = MemoryUtil.memAllocInt((12 * 3) * numShapes * Float.BYTES);
+            
             glBindVertexArray(g.vao);
             
             glBindBuffer(GL_ARRAY_BUFFER, g.vbo);
             glBufferData(GL_ARRAY_BUFFER, g.vertices.capacity(), GL_DYNAMIC_DRAW);
             
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, (3 * Float.BYTES), 0);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g.ibo);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, g.indices.capacity(), GL_DYNAMIC_DRAW);
+            
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, (numFloatsPerVertex * Float.BYTES), 0);
+            glVertexAttribPointer(1, 2, GL_FLOAT, false, (numFloatsPerVertex * Float.BYTES), (3 * Float.BYTES));
         
             glEnableVertexAttribArray(0);
+            glEnableVertexAttribArray(1);
             
             prevNumShapes = numShapes;
         }
@@ -59,22 +70,41 @@ class GeometryBatch {
     void batchEnd(ShaderProgram program) {
         if(numVertices > 0) {
             g.vertices.flip();
+            g.indices.flip();
             
             render(program);
             
             g.vertices.clear();
+            g.indices.clear();
             
             numVertices = 0;
         }
     }
     
     void drawGeometry(Geometry shape) {
-        for(int i = 0; i < 8; i++) {
-            Vector3f vertexPos = shape.vertices.get(i).position;
-            g.vertices.put(vertexPos.x).put(vertexPos.y).put(vertexPos.z);
+        try {
+            int indexOffset = (numVertices / numFloats) * Float.BYTES;
+            
+            for(int v = 0; v < 8; v++) {
+                Vector3f vertexPos = shape.vertices.get(v).position;
+                Vector2f texCoords = shape.vertices.get(v).texCoords;
+
+                g.vertices.put(vertexPos.x).put(vertexPos.y).put(vertexPos.z).put(texCoords.x).put(texCoords.y);
+            }
+            
+            shape.faces.forEach((index, face) -> {
+                int vert1 = indexOffset + face.indices[0];
+                int vert2 = indexOffset + face.indices[1];
+                int vert3 = indexOffset + face.indices[2];
+                
+                g.indices.put(vert1).put(vert2).put(vert3);
+            });
+
+            numVertices += numFloats;
+        } catch(BufferOverflowException e) {
+            Logger.setStackTrace(e);
+            Logger.log(LogLevel.SEVERE, "Geometry buffer experienced overflow");
         }
-        
-        numVertices += numFloats;
     }
     
 }
